6ef765fb555ce606e5d50bafffb200f8551c9e0b
logan <logan.testa@outlook.com>
2021-01-19 20:18:12 -0800

    Adjust header, logo styling

--- wordpress/wp-content/themes/twentyseventeen-child/functions.php
+++ wordpress/wp-content/themes/twentyseventeen-child/functions.php
@@ -1,5 +1,5 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=01142021' );
+    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=01192021' );
 });
