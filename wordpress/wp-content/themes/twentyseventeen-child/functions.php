9fc511cae80d2b7312984e8dd4cb682973ebbf3f
logan <logan.testa@outlook.com>
2022-07-11 20:49:12 -0700

    Adjust color for title in print

--- wordpress/wp-content/themes/twentyseventeen-child/functions.php
+++ wordpress/wp-content/themes/twentyseventeen-child/functions.php
@@ -2,5 +2,5 @@
 
 add_action( 'wp_enqueue_scripts', function() {
     wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=07112022' );
-    wp_enqueue_style( 'print-styles', "" . get_template_directory_uri() . '/print-styles.css?mod=07112022', array(), '', 'print' );
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=07112022', array(), '', 'print' );
 });
