ffc42d7a6517c876b350a2c1e5afd5c49e0c7bf9
logan <logan.testa@outlook.com>
2022-07-11 20:25:20 -0700

    Add print-styles

--- wordpress/wp-content/themes/twentyseventeen-child/functions.php
+++ wordpress/wp-content/themes/twentyseventeen-child/functions.php
@@ -1,5 +1,6 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=05222022' );
+    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=07112022' );
+    wp_enqueue_style( 'print-styles', "" . get_template_directory_uri() . '/print-styles.css?mod=07112022', array(), '', 'print' );
 });
