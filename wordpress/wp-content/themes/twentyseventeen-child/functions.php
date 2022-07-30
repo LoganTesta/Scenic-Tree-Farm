3f650a2d5842349316596cf96866f14792ed4cab
logan <logan.testa@outlook.com>
2022-07-29 20:39:27 -0700

    Improve print styling on the checkout page

--- wordpress/wp-content/themes/twentyseventeen-child/functions.php
+++ wordpress/wp-content/themes/twentyseventeen-child/functions.php
@@ -1,6 +1,6 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=07272022' );
-    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=07272022', array(), '', 'print' );
+    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=07292022' );
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=07292022', array(), '', 'print' );
 });
