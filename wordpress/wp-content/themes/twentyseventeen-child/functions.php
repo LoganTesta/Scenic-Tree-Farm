38dc47da0378939d9993d092956db7bfaf973c51
logan <logan.testa@outlook.com>
2024-12-06 22:19:34 -0800

    Add up arrow in the footer

--- wordpress/wp-content/themes/twentyseventeen-child/functions.php
+++ wordpress/wp-content/themes/twentyseventeen-child/functions.php
@@ -1,6 +1,6 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=11272024' );
-    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=11272024', array(), '', 'print' );
+    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=12062024' );
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=12062024', array(), '', 'print' );
 });
--- wordpress/wp-content/themes/twentyseventeen-child/template-parts/footer/site-info.php
+++ wordpress/wp-content/themes/twentyseventeen-child/template-parts/footer/site-info.php
@@ -9,7 +9,9 @@
  */
 
 ?>
+
 <div class="site-info">
+    <div id="scollToTop" class="scoll-to-top">&#8593;</div>
     <?php
     if (function_exists('the_privacy_policy_link')) {
         the_privacy_policy_link('', '<span role="separator" aria-hidden="true"></span>');
