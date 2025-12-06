26f471a2481427ec4dbf94b78f1411da676b19d3
logan <logan.testa@outlook.com>
2025-12-05 22:05:28 -0800

    Update styles

--- wordpress/wp-content/themes/twentyseventeen-child/functions.php
+++ wordpress/wp-content/themes/twentyseventeen-child/functions.php
@@ -1,8 +1,8 @@
 <?php
 
 add_action( 'wp_enqueue_scripts', function() {
-    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=09032025' );
-    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=09032025', array(), '', 'print' );
+    wp_enqueue_style( 'parent-style', "" . get_template_directory_uri() . '/style.css?mod=12052025' );
+    wp_enqueue_style( 'print-styles', "" . get_stylesheet_directory_uri() . '/assets/css/print-styles.css?mod=12052025', array(), '', 'print' );
     
     wp_register_script( 'javascript-functions', get_stylesheet_directory_uri() . '/assets/javascript/javascript-functions.js' );
     wp_enqueue_script( 'javascript-functions', get_stylesheet_directory_uri() . '/assets/javascript/javascript-functions.js' );  
